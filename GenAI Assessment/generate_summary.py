6d28743bd2b1a83f9a06e7755ca4775cff6ac1b7
aniketraj-sada <122963050+aniketraj-sada@users.noreply.github.com>
2023-09-20 08:01:00 +0000

    assessment completed

---
+++ GenAI Assessment/generate_summary.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import feedparser
+import trafilatura
+import os
+import openai
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import parsing
+import requests
+from bs4 import BeautifulSoup
+from langchain.llms import OpenAI
+from newspaper import Article
+
+os.environ['OPENAI_API_KEY'] = "sk"
+llm = OpenAI(temperature=0.6)
+
+def downloadArticleContent(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return article.text
+
+# Styling of streamlit application UI
+st.markdown(
+    """
+    <style>
+    /* Center the title and header */
+    .center {
+        text-align: center;
+    }
+    /* Style the title */
+    .title {
+        font-size: 28px;
+        font-weight: bold;
+        margin-bottom: 20px;
+        color: #333;
+    }
+    /* Style the header */
+    .header {
+        font-size: 20px;
+        font-weight: bold;
+        color: #333;
+    }
+    /* Style the input box */
+    .input {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    /* Style the summary div */
+    .summary {
+        margin-top: 20px;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    /* Style the links */
+    .link {
+        color: #007BFF;
+        text-decoration: underline;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Title
+st.markdown("<div class='center title'>RSS Feed Summarizer</div>", unsafe_allow_html=True)
+
+# Header
+st.markdown("<div class='center header'>Enter the link:</div>", unsafe_allow_html=True)
+
+# Input for the RSS feed link
+link = st.text_input("Enter the RSS Feed URL",key="rss_link", value="")
+
+feed = feedparser.parse(link)
+for entry in feed.entries[:10]:
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.subheader(entry.title)
+    st.write("Article Link: ", entry.link, unsafe_allow_html=True)
+
+    # Scrape article content
+    article_content = downloadArticleContent(entry.link)
+    
+    # Generate summary using Langchain
+    prompt_template_ = PromptTemplate(
+        input_variables=["article_content"],
+        template=f"Please provide a brief summary of the following article:\n\n{{article_content}}",
+        max_tokens=1024,
+    )
+
+    # Create the LLMChain and generate summary
+    chain = LLMChain(llm=llm, prompt=prompt_template)
+    summary = chain.run(article_content=article_content)
+    
+    # Display the summary with styling
+    st.markdown("<div class='summary'>", unsafe_allow_html=True)
+    st.write("Generated Summary: ", summary, unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # aniket-genai
 GenAI assessment 1
+# RSS Feed Summarizer with Streamlit
+
+This is a simple RSS feed summarizer built using Streamlit. It allows you to enter the URL of an RSS feed, retrieve the latest articles, and generate summaries for each article using OpenAI's API.
+
+## Prerequisites
+
+Before running the application, make sure you have Python installed on your system. You will also need to install the required packages. You can do this by running:
+
+`pip install -r requirements.txt`
+
+
+## Set up your OpenAI API key:
+   Get your OpenAI API key from [OpenAI](https://platform.openai.com/signup) and replace `"sk"` with your API key in the `os.environ['OPENAI_API_KEY']` line in the `generate_summary.py` file.
+
+## Run the Streamlit application using below command:
+ `streamlit run generate_summary.py`
+
+
+
